6a5fc1db848990cf7a4fa2209340a78688b10cbc
7182 <Briansubirat6@gmail.com>
2025-07-16 14:35:43 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/static/petezah/",
-    bare: "https://vplaza.org/bare",
+    bare: "https://vplaza.org/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/static/uv/uv.handler.js",
